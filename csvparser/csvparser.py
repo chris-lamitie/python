8a8f1040bb37f5dcf3332f85d577bbe3e506eab1
chris-lamitie <52215205+chris-lamitie@users.noreply.github.com>
2025-06-10 13:43:39 -0500

    Create csvparser.py

---
+++ csvparser/csvparser.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#################################################
+# Christopher Lamitie, 2025-06-10
+# a reporting log parser written for educational purposes
+#################################################
+print("Hello World!")
